f449a1b7cda4b6484614c0ea63663ce90bab87c6
Samir ASNOUN <samir.asnoun@gmail.com>
2012-11-06 16:15:49 +0100

    fix bug

--- recipes/database.rb
+++ recipes/database.rb
@@ -1,4 +1,9 @@
+
+node.set['cloudfoundry_common']['cf_session']['cf_id'] = node['cloudfoundry_cloud_controller']['cf_session']['cf_id']
+
+
 include_recipe "cloudfoundry-common"
+
 include_recipe "postgresql::server"
 
 postgresql_database node['cloudfoundry_cloud_controller']['database']['name'] do
